d4b8b1efa71020ef04145bc36973b8b610a4eebe
VIPAX-JIT <jatinbisen@JATINs-MacBook-Pro.local>
2025-10-06 10:25:01 +0530

    Your message

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5678;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const CALLBACK_URL = process.env.CALLBACK_URL;
 
 app.use(
   session({
@@ -19,6 +20,64 @@ app.use(
     cookie: { secure: false, maxAge: 24*60*60*1000 }, // secure: true only with https
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: CALLBACK_URL,
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(null, profile);
+      // });
+    }
+  )
+);
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+function isLoggedIn(req,res,next){
+  req.user?next():res.redirect("/");
+};
+app.get("/",(req,res)=>{
+  res.send('<a href="/auth/google">Login with GOOGLE</a>');
+});
+app.get("/auth/google",
+  passport.authenticate("google",{scope:["profile","email"]})
+);
+
+app.get("/auth/google/callback",
+  passport.authenticate("google",
+    {
+      failureRedirect: "/auth/failure",
+      successRedirect: "/protected",
+    }
+  )
+
+);
+app.get("/protected",isLoggedIn,(req,res)=>{
+  if (req.user) { 
+  res.send("YOU ARE AUTHENTICATED USER")
+  }else{
+    res.send("YO ARE NOT AUTHENTICATED")
+  }
+});
+
+app.get("/auth/failure",(req,res)=>{
+  res.send("AUTH MAI ERROR..!!");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
